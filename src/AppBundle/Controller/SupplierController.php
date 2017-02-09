4e743f20ab9c5d0935bf7be9af8c5e492e927189
Olaf Walkowiak <olafwal@googlemail.com>
2017-02-09 14:05:01 +0100

    Contract save, show qrcode

--- src/AppBundle/Controller/SupplierController.php
+++ src/AppBundle/Controller/SupplierController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Contract;
+use AppBundle\Form\ContractType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SupplierController extends Controller
 {
@@ -14,16 +17,49 @@ class SupplierController extends Controller
      */
     public function indexAction()
     {
-        return [];
+        $contractRepository = $this->getDoctrine()->getRepository(Contract::class);
+        $contracts = $contractRepository->findAll();
+        return ['contracts' => $contracts];
     }
 
     /**
-     * @Route("/contractEdit")
+     * @Route("/contractEdit/{id}")
      * @Template()
      */
-    public function contractEditAction()
+    public function contractEditAction(Request $request, $id = null)
     {
-        return [];
-    }
+        $contractRepository = $this->getDoctrine()->getRepository(Contract::class);
+        if ($id) {
+            $contract = $contractRepository->find($id);
+        } else {
+            $contract = new Contract();
+        }
+        $form = $this->createForm(ContractType::class, $contract);
 
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($contract);
+                $manager->flush();
+                return $this->redirect($this->generateUrl('app_supplier_index'));
+            }
+
+        }
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+    /**
+     * @Route("/contractShow/{id}")
+     * @Template()
+     */
+    public function contractShowAction($id)
+    {
+        $contract = $this->getDoctrine()->getRepository(Contract::class)->find($id);
+        return(
+            ['contract' => $contract]
+        );
+    }
 }
---
+++ src/AppBundle/Form/ContractType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\Form;
+
+use JMS\TranslationBundle\Model\Message;
+use JMS\TranslationBundle\Translation\TranslationContainerInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContractType extends AbstractType implements TranslationContainerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', null, ['label' => 'contract.name'])
+            ->add('description', null, ['label' => 'contract.desc'])
+            ->add('startDate', null, ['label' => 'contract.startDate'])
+            ->add('endDate', null, ['required' => false, 'label' => 'contract.endDate'])
+            ->add('active', null, ['required' => false, 'label' => 'contract.active'])
+            ->add('token', null, ['label' => 'contract.token']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Contract',
+            'translation_domain' => 'forms'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_contract';
+    }
+
+
+    /**
+     * Returns an array of messages.
+     *
+     * @return array<Message>
+     */
+    public static function getTranslationMessages()
+    {
+        return [
+            new Message('contract.name','form'),
+            new Message('contract.desc','form'),
+            new Message('contract.startDate','form'),
+            new Message('contract.endDate','form'),
+            new Message('contract.active','form'),
+            new Message('contract.token','form')
+        ];
+    }
+}
